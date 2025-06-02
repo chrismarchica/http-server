8873c03388585e3b877fa0cf9df36649d7b615ec
Chris Marchica <chrism36@bu.edu>
2025-06-02 16:24:49 -0400

    Now sends a response

--- client.c
+++ client.c
@@ -23,3 +23,37 @@ int create_client(int server_fd)
     }
     return client_fd;
 }
+void handle_client_request(int client_fd)
+{
+  char buffer[4096];
+  memset(buffer, 0, sizeof(buffer));
+  ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer)-1);
+  if(bytes_read < 0)
+  {
+    perror("read error");
+    close(client_fd);
+    return;
+  }
+  char method[16];
+  char path[256];
+  if(parse_request(buffer, method, sizeof(method), path, sizeof(path))){
+    printf("Method: %s\n", method);
+    printf("Path: %s\n", path);
+  }
+  else
+  {
+    printf("Could not parse\n");
+  }
+    //Response
+    const char *body = "Hello World!\n";
+    char response[1024];
+    snprintf(response, sizeof(response),
+	     "HTTP/1.1 200 OK\r\n"
+	     "Content-Type: text/plain\r\n"
+	     "Content-Length: %lu\r\n"
+	     "\r\n"
+	     "%s",
+	     strlen(body),body);
+    write(client_fd, response, strlen(response));
+    close(client_fd);
+}        
